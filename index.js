5277f06fbe51f063c43c6912a63ef1c0237d9793
ezraa1 <ezrakipchirchir1034@gmail.com>
2022-06-08 21:24:36 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide() {
+    const divide= (2000, 100)
+    return 2000/100
+    
+}
+let x =2
+const square = function (x){
+    return x*x
+}
+let a = 3
+let b = 4
+const add = function (a, b){
+    return a+b
+}
+
